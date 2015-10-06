8b6ce64445eca7a3096154ec5bdf53f76438c1cb
Kevin Damm <github@kevindamm.com>
2015-10-06 06:03:16 +0000

    Class composition relations for the board and related classes.

---
+++ src/board.ts
@@ -0,0 +1,18 @@
+/// <reference path="colors.ts" />
+/// <reference path="player.ts" />
+
+module wits {
+
+export class Board {
+  constructor (tiles: Tile[][], players: Player[]) {
+  }
+
+}
+
+export class Tile {
+  constructor (properties: {}) {
+  }
+
+}
+
+}  // module wits
